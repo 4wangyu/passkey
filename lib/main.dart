adacf00065e6c8a0031678fca46f3b0a0ebb7d20
4wangyu <4wangyu@gmail.com>
2020-06-28 17:57:21 +0800

    hide debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,12 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'passkey',
-      theme: ThemeData(
-        primarySwatch: Colors.teal,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: HomePage(),
-    );
+        title: 'passkey',
+        theme: ThemeData(
+          primarySwatch: Colors.teal,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: HomePage(),
+        debugShowCheckedModeBanner: false);
   }
 }
